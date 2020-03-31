7a1ba2f526580da524ffee45c2b3d38ca67fce61
DonAm1Go <62943482+DonAm1Go@users.noreply.github.com>
2020-03-31 15:01:02 +0200

    Update IdlerConfig.js

--- IdlerConfig.js
+++ IdlerConfig.js
@@ -4,8 +4,8 @@
     var botArray = [];
     
     config = {};
-    config.username = 'nick1';
-    config.password = 'pass1';
+    config.username = 'KOKOTKOCZ8';
+    config.password = 'Pompo123456';
     config.sharedSecret = ''; //Shared Secret(for 2FA only) https://github.com/Jessecar96/SteamDesktopAuthenticator/releases
     config.games = [730,440,570]
     configsArray.push(config);
